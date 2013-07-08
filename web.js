ff17ca0ddad7e3e9bed8deb7638c7488d1e4d786
Daniel McAuley <dmcauley4@gmail.com>
2013-07-08 00:17:18 +0000

    try again

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var mystring = fs.readFileSync("index.html","utf-8");
+
 
 app.get('/', function(request, response) {
-  response.send(mystring);
+  response.send(fs.readFileSync("index.html","utf-8"));
 });
 
 var port = process.env.PORT || 5000;
